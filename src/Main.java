9f159dcf4f0e3eb9bf510cb8fa28c4e166a5c532
hyeminililo <ng040115@gmail.com>
2024-11-20 09:47:45 +0900

    three

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
   public static void main(String[] args) {
-    //  자신의 학번과 이름을 출력하라
+    // 자신의 학번과 이름을 입력하라
     System.out.println("202214052 남궁혜민");
     System.out.println(Hello.sum(10));
     System.out.println(Hello.sum(100));
+    System.out.println(Hello.sum(1000));
   }
 }
